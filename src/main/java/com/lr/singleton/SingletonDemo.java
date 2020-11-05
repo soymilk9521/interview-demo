e157d918f36c76c81cf2ef26c6a47cf9d2aab194
soymilk9521 <soymilk9521@gmail.com>
2020-11-05 22:17:37 +0900

    volatile singleton

---
+++ src/main/java/com/lr/singleton/SingletonDemo.java
@@ -0,0 +1,52 @@
+package com.lr.singleton;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author LR
+ * @since 2020-11-05 9:21
+ */
+public class SingletonDemo {
+
+    private static SingletonDemo instance;
+
+    private  SingletonDemo() {
+        System.out.println(Thread.currentThread().getName() + "\t 我是构造方法 SingletonDemo()");
+    }
+
+    // public static synchronized SingletonDemo getInstance() { // 使用synchronize的关键字可以解决多线程单例模式问题，但不推荐！
+    public static SingletonDemo getInstance() {
+        if (instance == null) {
+            synchronized (SingletonDemo.class){
+                if (instance == null) {
+                    instance = new SingletonDemo(); 
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        // 单线程操作单例模式：没有问题
+//        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+//        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+//        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+//
+//
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+
+
+        // 多线程操作单例模式：存在问题
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                SingletonDemo.getInstance(); // 多线程环境下构造方法执行多次
+            }, String.valueOf(i)).start();
+        }
+    }
+
+
+}
--- src/main/java/com/lr/volatiledemo/VolatileDemo.java
+++ src/main/java/com/lr/volatiledemo/VolatileDemo.java
@@ -1,6 +1,7 @@
 package com.lr.volatiledemo;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * <p>
@@ -14,24 +15,38 @@ import java.util.concurrent.TimeUnit;
  * 2. 验证volatile不具备原子性
  *    2.1 原子性指的是，不可分割，完整性，即某个线程在做具体业务时，中间不可以加塞，不能被分割，需要整体完整
  *        要么同时成功要么同时失败。
+ *    2.2 volatile 不保证原子性
+ *
+ *    2.3 volatile为什么不能保证原子性，字节码分析
+ *
+ *    2.4 如果解决原子性问题？
+ *        - 加synchronized代码块
+ *        - 使用juc包下的AtomicInteger
  */
 public class VolatileDemo {
     public static void main(String[] args) {
         // volatile可见性演示
         // volatileVisibility();
+        // volatile 非原子性演示
+        volatileNonAtomic();
+
+    }
+
+    private static void volatileNonAtomic() {
         Mydata data = new Mydata();
         for (int i = 0; i < 20; i++) {
             new Thread(()->{
                 for (int j = 0; j < 1000; j++) {
                     data.increment();
+                    data.atomicAdd();
                 }
             }, String.valueOf(i)).start();
         }
         while (Thread.activeCount() > 2){
             Thread.yield();
         }
-        System.out.println("The num value is " + data.num);
-
+        System.out.println("int type, The num value is " + data.num);
+        System.out.println("AtomicInteger type, The atomicInteger value is " + data.atomicInteger);
     }
 
     /***
@@ -60,11 +75,19 @@ public class VolatileDemo {
 class Mydata{
     // volatile jvm轻量级的同步机制，保证线程间的可见性
     volatile int num = 0;
+    AtomicInteger atomicInteger = new AtomicInteger();
+
     public void addto60(){
         this.num = 60;
     }
 
+    // public synchronized void increment() { // 解决原子性问题，但是杀鸡用牛刀
     public void increment() {
         this.num++;
+    }
+
+    // AtomicInteger提供原子操作的Integer的类
+    public void atomicAdd() {
+        atomicInteger.getAndIncrement();
     }
 }
