4d2c9867db5fa8be96e2f293bd7793e1d4096792
soymilk9521 <soymilk9521@gmail.com>
2020-11-15 10:21:08 +0900

    custom ThreadPoolExecutor

--- src/main/java/com/lr/thread/MyThreadPoolDemo.java
+++ src/main/java/com/lr/thread/MyThreadPoolDemo.java
@@ -13,6 +13,33 @@ import java.util.concurrent.*;
  */
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
+        // testJKDExecutors();
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                1l,
+                 TimeUnit.SECONDS,
+                 new LinkedBlockingDeque<Runnable>(3),
+                 Executors.defaultThreadFactory(),
+                 // new ThreadPoolExecutor.AbortPolicy()); // 超出corePoolSize+workQueue的和的任务直接抛出异常
+                 // new ThreadPoolExecutor.CallerRunsPolicy()); // 超出corePoolSize+workQueue的和的任务交给调用者main运行
+                 // new ThreadPoolExecutor.DiscardOldestPolicy()); // 8和9代替前面某个等待时间最久的任务，直接执行
+                 new ThreadPoolExecutor.DiscardPolicy()); // 8和9任务直接被抛弃
+        try {
+            for (int i = 0; i < 10; i++) {
+                final int tempInt = i;
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务 ----> " + tempInt);
+                });
+            }
+        }finally {
+            threadPool.shutdown();
+        }
+
+
+    }
+
+    private static void testJKDExecutors() {
         System.out.println(Runtime.getRuntime().availableProcessors());
         // 父接口
         // Executor executor = null;
@@ -29,6 +56,5 @@ public class MyThreadPoolDemo {
         }finally {
             service.shutdown();
         }
-
     }
 }
